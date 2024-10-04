7345e7b7d3d9d52c22a8d32bacc8faa0534098ee
diasbouk <boukdsaid@gmail.com>
2024-10-05 00:30:57 +0100

    Task 1

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,10 +3,9 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true;
 
   if (trueOrFalse) {
-    const task = true;  // eslint-disable-line no-unused-vars
-    const task2 = false;  // eslint-disable-line no-unused-varse;
+    const task = true; // eslint-disable-line no-unused-vars
+    const task2 = false; // eslint-disable-line no-unused-vars
   }
 
   return [task, task2];
 }
-
