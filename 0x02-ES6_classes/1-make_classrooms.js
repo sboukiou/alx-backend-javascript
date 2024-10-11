b3714073c97b32ad452c74b48afa400ced04acc5
diasbouk <boukdsaid@gmail.com>
2024-10-11 23:14:26 +0100

    Fix import

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,8 +1,9 @@
-import { ClassRoom } from './0-classroom';
+import ClassRoom from './0-classroom';
 
 export default function initializeRooms() {
-  const room1 = new ClassRoom(19);
-  const room2 = new ClassRoom(20);
-  const room3 = new ClassRoom(43);
-  return [room1, room2, room3];
+  return [
+    new ClassRoom(19),
+    new ClassRoom(20),
+    new ClassRoom(43),
+  ];
 }
