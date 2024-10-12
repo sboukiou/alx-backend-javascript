637b0966ee4e4077b6138acc99d69672c85c0af7
diasbouk <boukdsaid@gmail.com>
2024-10-12 10:15:36 +0100

    Pricing class . currency + amount + maths

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,40 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(amount) {
+    if (amount instanceof Number) {
+      this._amount = amount;
+    } else {
+      throw new TypeError('Amount must a number');
+    }
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(currency) {
+    if (currency instanceof Currency) {
+      this._currency = currency;
+    } else {
+      throw new TypeError('currency must a Class Currency instance');
+    }
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency._name} (${this._currency._code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
